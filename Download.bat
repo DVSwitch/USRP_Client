332ed4ed43548fa5e1d01f5e3169aeea45120a83
André Franco <30467158+afranco8@users.noreply.github.com>
2020-01-17 19:02:24 +0000

    Update Download.bat

--- Download.bat
+++ Download.bat
@@ -2,18 +2,18 @@
 setlocal
 
 set "psCommand="(new-object -COM 'Shell.Application')^
-.BrowseForFolder(0,'Seleccione a pasta onde se encontra o USRP Client',0,0).self.path""
+.BrowseForFolder(0,'Select USRP Client folder',0,0).self.path""
 
 for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "folder=%%I"
 
 setlocal enabledelayedexpansion
 
-rem Download da library necessária:
+rem Download library:
 SET "FILE_URL=https://files.pythonhosted.org/packages/94/3e/430d4e4e24e89b19c1df052644f69e03d64c1ae2e83f5a14bd365e0236de/PyAudio-0.2.11-cp27-cp27m-win_amd64.whl"
 SET "SAVING_TO=!folder!"\PyAudio-0.2.11-cp37-cp37m-win_amd64.whl"
 CALL :DOWNLOAD_FILE "%FILE_URL%" "%SAVING_TO%"
 :DOWNLOAD_FILE
-    rem Comando para fazer download:
+    rem Download command:
     bitsadmin /transfer Download /download /priority normal %1 %2
 
 cd !folder!
@@ -23,7 +23,7 @@ pip install Pillow
 pip install requests
 echo ""
 echo "----------------------------------------------------------------------------------"
-echo "Por favor, edite o ficheiro pyUC.ini com as suas credenciais (Indicativo, DMR ID)
+echo "Installation complete. You should now edit pyUC.ini before using USRP Client"
 
 Timeout /t 5
 exit
