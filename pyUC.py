0225dd952c1fcbb162234042fd06b383fc03f8b5
Michael Zingman <Michael.Zingman@cmg.com>
2020-12-12 10:01:20 -0500

    Set metadata on register.

--- pyUC.py
+++ pyUC.py
@@ -463,6 +463,7 @@ def rxAudioStream():
                 if (audio[0:4] == REG):
                     if (audio[4:6] == OK):
                         connected_msg.set(STRING_REGISTERED)
+                        sendMetadata()
                         requestInfo()
                         if in_index == -1:
                             transmitButton.configure(state='disabled')
@@ -761,8 +762,11 @@ def requestInfo():
 # 
 ###################################################################################
 def sendMetadata():
-    metadata = ""
-    sendRemoteControlCommandASCII(metadata)
+    dmr_id = subscriber_id.get()
+    call = bytes(my_call, 'ASCII')+bytes(chr(0), 'ASCII')
+    tlvLen = 3 + 4 + 3 + 1 + 1 + len(my_call) + 1                      # dmrID, repeaterID, tg, ts, cc, call, 0
+    cmd = struct.pack("BBBBBBBBBBBBBB", TLV_TAG_SET_INFO, tlvLen, ((dmr_id >> 16) & 0xff),((dmr_id >> 8) & 0xff),(dmr_id & 0xff),0,0,0,0,0,0,0,0,0)[0:14] + call
+    sendUSRPCommand(cmd, USRP_TYPE_TEXT)
 
 ###################################################################################
 # Set the size (number of bits) of each AMBE sample
